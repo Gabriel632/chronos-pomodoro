9b87783f800e22bdb9e27e361500068e427d358f
Gabriel Henrique <gabriel.hdda@gmail.com>
2025-07-09 22:01:21 -0300

    progressão do curso até a aula 29

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/theme.css';
+import './styles/global.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export function App() {
+	return (
+		<>
+			<Container>
+				<Logo />
+			</Container>
+
+			<Container>
+				<Menu />
+			</Container>
+
+			<Container>
+				<CountDown />
+			</Container>
+
+			<Container>
+				<form className='form'>
+					<div className='formRow'>
+						<DefaultInput
+							id='meuInput'
+							type='text'
+							labelText='task'
+							placeholder='Digite algo'
+						/>
+					</div>
+
+					<div className='formRow'>
+						<p>Lorem, ipsum dolor sit amet.</p>
+					</div>
+
+					<div className='formRow'>
+						<Cycles />
+					</div>
+
+					<div className='formRow'>
+						<button>Enviar</button>
+					</div>
+				</form>
+			</Container>
+
+			<Container>
+				<Heading>FOOTER</Heading>
+			</Container>
+		</>
+	);
 }
-
-export default App
